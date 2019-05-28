4a07a956b82473384fdc87a78085b78e461134dc
Ji Zhi Qian <jizhi.qian@jimubox.com>
2019-05-28 20:12:10 +0800

    dup

--- src/test/java/com/hogwarts/webui/TestBaiDu.java
+++ src/test/java/com/hogwarts/webui/TestBaiDu.java
@@ -34,4 +34,28 @@ public class TestBaiDu extends WebUIBase {
         String browserTitle = driver.getTitle();
         Assert.assertTrue("检查页面标题包含‘" + checkString + "’", browserTitle.contains(checkString + "——————————"));
     }
+
+    @Test
+    public void baiduSearch2() throws Exception{
+        testcaseName = "百度搜索UI自动化测试";
+        String checkString = "selenium";
+
+        logger.info("开始运行自动化测试：" + testcaseName);
+
+        //浏览器中打开百度
+        logger.info("浏览器中打开百度网站");
+        navigation.to("http://www.baidu.com");
+        wait2s();
+
+        //输入搜索内容：Selenium
+        WebUITasks.inputText(checkString,driver);
+        wait2s();
+
+        //单击搜索按钮
+        WebUITasks.clickSearchBtn(driver);
+        wait2s();
+
+        String browserTitle = driver.getTitle();
+        Assert.assertTrue("检查页面标题包含‘" + checkString + "’", browserTitle.contains(checkString));
+    }
 }
