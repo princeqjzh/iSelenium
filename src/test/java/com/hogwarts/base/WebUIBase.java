7bfff3e10944cf4fd94daea7a71329f2a62e69e6
Ji Zhi Qian <jizhi.qian@jimubox.com>
2021-01-21 17:44:57 +0800

    Print out exception msg

--- src/test/java/com/hogwarts/base/WebUIBase.java
+++ src/test/java/com/hogwarts/base/WebUIBase.java
@@ -94,7 +94,7 @@ public abstract class WebUIBase {
             prop.load(in);
             in.close();
         } catch (IOException ioex) {
-            ioex.printStackTrace();
+            logger.error(ioex.getMessage());
             logger.error("Load config file fail, please check " + path + " to confirm if the "
                     + propFileName + " file exist!");
         }
